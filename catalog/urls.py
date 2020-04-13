768abc70f590ee15d526cec7327f1fb02a28b8e6
Stormtrooper <mr.fryd@mail.ru>
2020-04-13 13:53:36 +0300

    ff

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'^allbooks/$', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
-
 ]
 
